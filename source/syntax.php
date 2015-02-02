6df8c5ea59edf2a8c9f732170323199ac6574df7
otriesch <develop@otri.de>
2015-02-02 01:48:35 +0000

    Update syntax.php

--- source/syntax.php
+++ source/syntax.php
@@ -1,62 +1,92 @@
 <?php
-    /**
-     * Plugin: BitbucketRepFiles
-     * 
-     * @license GPL 2 (http://www.gnu.org/licenses/gpl.html)
-     * @author Olaf Trieschmann <develop@otri.de> 
-     * 
-     * Thanks to Igor Kromin's (http://www.igorkromin.net) plugin "GitLab Commits", which was used as a starting point!
-     */
+/**
+ * Plugin: BitbucketRepFiles
+ * 
+ * @license GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author Olaf Trieschmann <develop@otri.de> 
+ * 
+ * Thanks to Igor Kromin's (http://www.igorkromin.net) plugin "GitLab Commits", which was used as a starting point!
+ */
+// must be run within Dokuwiki
+if(!defined('DOKU_INC')) die();
+
+if(!defined('DOKU_INC')) define('DOKU_INC',realpath(dirname(__FILE__).'/../../').'/');
+if(!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
+require_once(DOKU_PLUGIN.’syntax.php');
     
-    if(!defined('DOKU_INC')) define('DOKU_INC',realpath(dirname(__FILE__).'/../../').'/');
-    if(!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
-    require_once(DOKU_PLUGIN.’syntax.php');
+class syntax_plugin_bitbucketrepfiles extends DokuWiki_Syntax_Plugin {
     
-    class syntax_plugin_bitbucketrepfiles extends DokuWiki_Syntax_Plugin {
-        
-        function getInfo() {
-            return array('author' => 'Olaf Trieschmann',
-                         'email'  => 'develop@otri.de',
-                         'date'   => '2015-02-01′,
-                         'name'   => 'BitbucketRepFiles Plugin',
-                         'desc'   => 'Dokuwiki plugin to show the files in a Bitbucket repository',
-                         'url'    => 'https://github.com/otriesch/itemtable/raw/master/bitbucketrepfiles.zip');
-        }
-        
-        function getType(){ return ’substition'; }
-        function getPType(){ return 'block'; }
-        function getSort(){ return 100; }
-        
-        function connectTo($mode) {
-            $this->Lexer->addSpecialPattern('[bitbucketrepfiles]',$mode,'plugin_bitbucketrepfiles');
-        }
-        
-        function handle($match, $state, $pos, &$handler){
-            return array($match, $state, $pos);
-        }
-        
-        function render($mode, &$renderer, $data) {
-            if($mode == 'xhtml'){
-                $pageurl = "https://bitbucket.org/api/1.0/repositories/otrima/randt-ansible/branches/";
-                $ch=curl_init();
-                curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-                curl_setopt($ch, CURLOPT_URL, $pageurl);
-                $html = curl_exec($ch);
-                curl_close($ch);
-                
-                $json = new JSON();
-                $array  = $json->decode($html);
-                
-                $renderer->doc .= '<table class="pagelist" style="width:725px"><tr><th class="page">ID</th><th class="page">Author</th><th class="page">Title</th><th class="date">Date</th></tr>';
-                foreach($array as &$val) {
-                    $renderer->doc .= '<tr><td class="desc">' . $val->short_id . '</td><td class="desc">' . $val->author_name . '</td><td class="desc">' . $val->title . '</td><td class="date">' . $val->created_at . '</td></tr>';
-                }
-                $renderer->doc .= '</table>';
-                
-                return true;
-            }
-            return false;
-        }
+    function getInfo() {
+        return array('author' => 'Olaf Trieschmann',
+                     'email'  => 'develop@otri.de',
+                     'date'   => '2015-02-01′,
+                     'name'   => 'BitbucketRepFiles Plugin',
+                     'desc'   => 'Dokuwiki plugin to show the files in a Bitbucket repository',
+                     'url'    => 'https://github.com/otriesch/itemtable/raw/master/bitbucketrepfiles.zip');
     }
+    
+    function getType(){ return 'substition'; }
+    function getPType(){ return 'block'; }
+    function getSort(){ return 100; }
+    
+    /**
+     * Connect pattern to lexer
+     */
+    public function connectTo($mode) {
+        $this->Lexer->addSpecialPattern('\{\{bitbucketrepfiles>.+?\}\}',$mode,'plugin_bitbucketrepfiles');
+    }
+    
+    public function handle($match, $state, $pos, Doku_Renderer $handler){
+		$start = strlen('{{bitbucketrepfiles>');
+		$end = -2;
+
+		$params = substr($match, $start, $end);
+		$params = preg_replace('/\s{2,}/', '', $params);
+		$params = preg_replace('/\s[=]/', '=', $params);
+		$params = preg_replace('/[=]\s/', '=', $params);
+
+		$data = array();
+		foreach(explode('&', $params) as $param)
+		{
+			$val = explode('=', $param);
+			$data[$val[0]] = $val[1];
+		}
+        return $return;
+
+//        return array($data, $state, $pos);
+    }
+    
+    /**
+     * Handles the actual output creation.
+     *
+     * @param   $mode       string        output format being rendered
+     * @param   $renderer   Doku_Renderer the current renderer object
+     * @param   $data       array         data created by handler()
+     * @return  boolean                   rendered correctly?
+     */
+    public function render($mode, Doku_Renderer $renderer, $data) {
+        if($mode == 'xhtml') && isset($data['URL']){
+            $pageurl = $data['URL']
+//            $pageurl = "https://bitbucket.org/api/1.0/repositories/otrima/randt-ansible/branches/";
+            $ch=curl_init();
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt($ch, CURLOPT_URL, $pageurl);
+            $html = curl_exec($ch);
+            curl_close($ch);
+            
+            $json = new JSON();
+            $array  = $json->decode($html);
+            
+            $renderer->doc .= '<table class="pagelist" style="width:725px"><tr><th class="page">ID</th><th class="page">Author</th><th class="page">Title</th><th class="date">Date</th></tr>';
+            foreach($array as &$val) {
+                $renderer->doc .= '<tr><td class="desc">' . $val->short_id . '</td><td class="desc">' . $val->author_name . '</td><td class="desc">' . $val->title . '</td><td class="date">' . $val->created_at . '</td></tr>';
+            }
+            $renderer->doc .= '</table>';
+            
+            return true;
+        }
+        return false;
+    }
+}
     
 ?>
